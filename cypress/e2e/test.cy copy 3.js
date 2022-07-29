411d123e5de6117516b7ea6a7e1f87f04f9e8f03
Gowsigan <ochathevan_gowsigan@lilly.com>
2022-07-29 10:49:15 +0530

    test

---
+++ cypress/e2e/test.cy copy 3.js
@@ -0,0 +1,5 @@
+describe('My First Test', () => {
+    it('Google.com copy 3 test', () => {
+      cy.visit('https://google.com/');
+    })
+  })
---
+++ cypress/e2e/test.cy copy.js
@@ -0,0 +1,5 @@
+describe('My First Test', () => {
+    it('Google.com copy 1 test', () => {
+      cy.visit('https://google.com/');
+    })
+  })
